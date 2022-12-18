fd2c933aa9fc95bd1b2a8828606d1fabfca78848
gddezero <gdde_0@hotmail.com>
2022-12-18 18:08:41 +0800

    Revert "[hotfix][core] Add Configuration.fromMap"
    This reverts commit ed4d3b12a74f6e2cdcb1f55996ba56ad3dbda3f9.

--- flink-core/src/test/java/org/apache/flink/configuration/DelegatingConfigurationTest.java
+++ flink-core/src/test/java/org/apache/flink/configuration/DelegatingConfigurationTest.java
@@ -20,6 +20,7 @@ package org.apache.flink.configuration;
 
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Set;
@@ -32,15 +33,14 @@ public class DelegatingConfigurationTest {
 
     @Test
     public void testIfDelegatesImplementAllMethods()
-            throws IllegalArgumentException {
+            throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 
         // For each method in the Configuration class...
         Method[] confMethods = Configuration.class.getDeclaredMethods();
         Method[] delegateMethods = DelegatingConfiguration.class.getDeclaredMethods();
 
         for (Method configurationMethod : confMethods) {
-            final int mod = configurationMethod.getModifiers();
-			if (!Modifier.isPublic(mod) || Modifier.isStatic(mod)) {
+            if (!Modifier.isPublic(configurationMethod.getModifiers())) {
                 continue;
             }
 
